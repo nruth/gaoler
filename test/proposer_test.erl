de3310ec53c1f0a8ef819903a7e9c6f9c1d35078
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-11-02 23:12:02 +0100

    re-macroing proposer tests to hide record api

--- test/proposer_test.erl
+++ test/proposer_test.erl
@@ -2,18 +2,21 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("proposer_state.hrl").
 
+-define(MOCKCOMMS,  #state{acceptors_proxy=nspy:mock()}).
 -define(PROMISES(N),#state{promises=N}).
+-define(ROUND(N),   #state{round=N}).
+-define(VALUE(V),   #state{value=V}).
 
 %% happy case: no other proposers and round 1 succeeds
 first_promise_received_test() ->
     Round = 1,
-    InitialState = ?PROMISES(0)#state{round=Round},
+    InitialState = ?MOCKCOMMS?PROMISES(0)?ROUND(Round),
     Result = proposer:awaiting_promises({promised, Round}, InitialState),
     ?assertMatch({next_state, awaiting_promises, ?PROMISES(1)}, Result).
 
 success_case_move_to_next_state_test() ->
     Round = 1,
-    InitialState = ?PROMISES(2)#state{round=Round},
+    InitialState = ?MOCKCOMMS?PROMISES(2)?ROUND(Round),
     Result = proposer:awaiting_promises({promised, Round}, InitialState),
     ?assertMatch({next_state, awaiting_accepts, ?PROMISES(3)}, Result).
 
@@ -21,7 +24,7 @@ success_case_move_to_next_state_test() ->
 %% sad case: someone else has been promised a higher round
 higher_promise_received_test() ->
     Round = 1,
-    InitialState = ?PROMISES(0)#state{round=Round},
+    InitialState = ?MOCKCOMMS?PROMISES(0)?ROUND(Round),
     Result = proposer:awaiting_promises({promised, 100}, InitialState),
     ?assertMatch({next_state, aborted, _}, Result).
 
