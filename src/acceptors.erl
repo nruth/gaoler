cdd424af9611d5f28718ac4e8c9a38b136f5fa3f
Marcus Ljungblad <marcus@ljungblad.nu>
2011-11-08 10:40:04 +0100

    reverted to cleaner, but duplicated code

--- src/acceptors.erl
+++ src/acceptors.erl
@@ -5,15 +5,19 @@
         ]).
 
 -export([
-	 send_promise/2
+	 send_promise/2,
+	 send_accept/3
 	]).
+
 
 send_promise_request(Round) ->
     [spawn(fun() -> acceptors:send_promise(Acceptor, Round) end) ||
 	Acceptor <- gaoler:get_acceptors()],
     ok.
 
-send_accept_request(_Round, _Value) ->
+send_accept_request(Round, Value) ->
+    [spawn(fun() -> acceptors:send_accept(Acceptor, Round, Value) end) ||
+	Acceptor <- gaoler:get_acceptors()],
     ok.
 
 
@@ -21,3 +25,7 @@ send_accept_request(_Round, _Value) ->
 send_promise(Acceptor, Round) ->    
     Reply = acceptor:prepare(Acceptor, Round),
     proposer:promised(Reply).
+
+send_accept(Acceptor, Round, Value) ->
+    Reply = acceptor:accept(Acceptor, Round, Value),
+    proposer:accepted(Reply).
