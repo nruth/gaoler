707ac812b135077a724d86e279ae32a5457da495
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-11-09 19:13:42 +0100

    tests that learner count changes only the given key

--- test/learner_test.erl
+++ test/learner_test.erl
@@ -2,6 +2,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -define (STATE (Round, Value, AcceptCount), [{{Round, Value}, AcceptCount}]).
+-define (JOIN_STATE (State1, State2), lists:append(State1, State2)).
+-define (ASSERT_SAME_ELEMENTS (List1, List2), ?assertEqual(lists:keysort(1,List1), lists:keysort(1,List2))).
 
 first_accept_received_for_round_value_pair_counts_1_test() ->
     Round = 100, Value = foo, AcceptCount = 0,
@@ -14,6 +16,12 @@ accept_received_for_known_value_increases_count_test() ->
     InitialState = ?STATE(Round, Value, AcceptCount),
     Result = learner:handle_event({accepted, Round, Value}, InitialState),
     ?assertEqual({ok, ?STATE(Round, Value, AcceptCount + 1)}, Result).
+
+accept_received_only_increases_its_own_value_test() ->
+    Round = 1, AcceptCount = 1,
+    InitialState = ?STATE(Round, first_value, AcceptCount),
+    {ok, NewState} = learner:handle_event({accepted, Round, second_value}, InitialState),
+    ?ASSERT_SAME_ELEMENTS(NewState, ?JOIN_STATE(InitialState, ?STATE(Round, second_value, 1))).
 
 % Tests with Meck stub and cleanup of learners broadcast proxy
 broadcast_result_test_() ->
