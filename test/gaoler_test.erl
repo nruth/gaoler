ab220e6563292e7315b11767403ad6ea76079552
Marcus Ljungblad <marcus@ljungblad.nu>
2011-11-10 00:59:27 +0100

    tests for client subscriptions

--- test/gaoler_test.erl
+++ test/gaoler_test.erl
@@ -4,12 +4,12 @@
 
 -define(SUBSCRIBER(Fun), #state{subscribers = [Fun]}).
 
-subscribe_fun_test() ->
+client_subscribe_fun_test() ->
     Fun = fun(Value) -> io:format("delivered ~p", [Value]) end,
     Result = gaoler:handle_call({subscribe, Fun}, nil, #state{}),
     ?assertMatch({reply, ok, ?SUBSCRIBER(Fun)}, Result).
 
-subscribe_module_function_test() ->
+client_subscribe_module_function_test() ->
     Module = dummy,
     Function = deliver,
     meck:new(Module),
@@ -19,7 +19,19 @@ subscribe_module_function_test() ->
     ?assertMatch({reply, ok, ?SUBSCRIBER({Module, Function})}, Result),
     meck:unload(Module).
 				
-unsubscribe_test() ->
+client_unsubscribe_test() ->
     Fun = fun(Value) -> io:format("delivered ~p", [Value]) end,
     Result = gaoler:handle_call({unsubscribe, Fun}, nil, ?SUBSCRIBER(Fun)),
     ?assertMatch({reply, ok, #state{subscribers=[]}}, Result).
+
+when_deliver_message_to_client_check_callback_is_called_test() ->
+    Module = dummy,
+    Function = deliver,
+    meck:new(Module),
+    meck:expect(Module, Function, 1, ok),
+    InitialState = ?SUBSCRIBER({Module, Function}),
+    Result = gaoler:handle_cast({deliver, value}, nil, InitialState),
+    timer:sleep(10),
+    ?assert(meck:called(dummy, deliver, '_')),
+    meck:unload(Module).
+    
