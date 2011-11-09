db6114c59c2be3e3f8bad7014907069fd9fcc88a
Marcus Ljungblad <marcus@ljungblad.nu>
2011-11-10 00:56:11 +0100

    tests for client subscriptions

--- src/gaoler.erl
+++ src/gaoler.erl
@@ -1,12 +1,17 @@
 -module(gaoler).
 -behaviour(gen_server).
 
+-include_lib("gaoler_state.hrl").
+
 %% API
 -export([
 	 start_link/0, 
 	 join/0,
 	 get_acceptors/0,
-	 stop/0
+	 stop/0,
+	 subscribe/1,
+	 unsubscribe/1,
+	 deliver/1
 	]).
 
 %% gen_server callbacks
@@ -19,10 +24,13 @@
 	 code_change/3
 	]).
 
+%% internal child processes
+-export([
+	 deliver_message/2
+	]).
+
 -define(SERVER, ?MODULE). 
 -define(REQUIRED_MAJORITY, 3).
-
--record(state, {}).
 
 %%%===================================================================
 %%% API
@@ -30,6 +38,17 @@
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+subscribe(Callback) when is_function(Callback, 1) ->
+    gen_server:call(?SERVER, {subscribe, Callback});
+subscribe({_Module, _Function}=Callback) ->
+    gen_server:call(?SERVER, {subscribe, Callback}).
+
+unsubscribe(Callback) ->
+    gen_server:call(?SERVER, {unsubscribe, Callback}).
+
+deliver(Value) ->
+    gen_server:cast(?SERVER, {deliver, Value}).
 
 join() ->
     gen_server:abcast(?SERVER, {join, node()}). 
@@ -54,11 +73,33 @@ init([]) ->
 
 handle_call(get_acceptors, _From, State) ->
     Reply = [{acceptor, Node} || Node <- [node()|nodes()]],
-    {reply, Reply, State}.
-
+    {reply, Reply, State};
+handle_call({subscribe, {Module, Function}=ModFun}, _From, State) ->
+    {Reply, NewState} = 
+	case erlang:function_exported(Module, Function, 1) of
+	    true ->
+		{ok, add_subscriber(ModFun, State)};
+	    false ->
+		{{error, not_exported}, State}
+	end,
+    {reply, Reply, NewState};
+handle_call({subscribe, Callback}, _From, State) ->
+    NewState = add_subscriber(Callback, State),
+    {reply, ok, NewState};
+handle_call({unsubscribe, Callback}, _From, State) ->
+    NewState = State#state{subscribers = 
+			       lists:delete(Callback, 
+					    State#state.subscribers)},
+    {reply, ok, NewState}.
 
 handle_cast({join, Node}, State) ->
     erlang:monitor_node(Node, true),
+    {noreply, State};
+handle_cast({deliver, Value}, State) ->
+    spawn_link(fun() -> 
+		       ?MODULE:deliver_message(Value, 
+					       State#state.subscribers) 
+	       end),
     {noreply, State};
 handle_cast(stop, State) ->
     {stop, normal, State};
@@ -75,6 +116,9 @@ handle_info({nodedown, Node}, State) ->
 	    ok
     end,
     {noreply, State};
+handle_info({'EXIT', _FromPid, Reason}, State) ->
+    io:format("Malformed callback: ~p~n", [Reason]),
+    {noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -89,3 +133,14 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
+deliver_message(_, []) -> 
+    ok;
+deliver_message(Value, [{Module,Function}|Tail]) -> 
+    Module:Function(Value),
+    deliver_message(Value, Tail);
+deliver_message(Value, [Function|Tail]) when is_function(Function) -> 
+    Function(Value),
+    deliver_message(Value, Tail).
+
+add_subscriber(Callback, State) -> 
+    State#state{subscribers = [Callback|State#state.subscribers]}.
--- test/gaoler_test.erl
+++ test/gaoler_test.erl
@@ -1,6 +1,25 @@
 -module(gaoler_test).
 -include_lib("eunit/include/eunit.hrl").
+-include_lib("gaoler_state.hrl").
 
-% start gaoler
-% join group
-% receive other joins
+-define(SUBSCRIBER(Fun), #state{subscribers = [Fun]}).
+
+subscribe_fun_test() ->
+    Fun = fun(Value) -> io:format("delivered ~p", [Value]) end,
+    Result = gaoler:handle_call({subscribe, Fun}, nil, #state{}),
+    ?assertMatch({reply, ok, ?SUBSCRIBER(Fun)}, Result).
+
+subscribe_module_function_test() ->
+    Module = dummy,
+    Function = deliver,
+    meck:new(Module),
+    meck:expect(Module, Function, 1, ok),
+    Result = gaoler:handle_call({subscribe, {Module, Function}}, 
+				nil, #state{}),
+    ?assertMatch({reply, ok, ?SUBSCRIBER({Module, Function})}, Result),
+    meck:unload(Module).
+				
+unsubscribe_test() ->
+    Fun = fun(Value) -> io:format("delivered ~p", [Value]) end,
+    Result = gaoler:handle_call({unsubscribe, Fun}, nil, ?SUBSCRIBER(Fun)),
+    ?assertMatch({reply, ok, #state{subscribers=[]}}, Result).
