73c6c0708e0ab11d7e857313ef1d3274caffcc8e
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-11-10 14:47:00 +0100

    testing learners broadcast

--- src/learners.erl
+++ src/learners.erl
@@ -3,6 +3,10 @@
 -export ([broadcast_result/1]).
 
 broadcast_result(Value) ->
-    [spawn(fun() -> gen_event:notify(Learner, {result, Value}) end) ||
-	Learner <- gaoler:get_learners()],
+    case gaoler:learners() of
+        [] -> erlang:errors(no_learners);
+        Learners ->
+            [spawn(fun() -> gen_event:notify(Learner, {result, Value}) end) ||
+        	Learner <- Learners]
+    end,
     ok.
