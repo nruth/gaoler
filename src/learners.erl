4e9e38a0bd7566e6bb29c58dade94c33ef97dc55
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-11-16 17:29:34 +0100

    enabling learners proxy to broadcast accepts

--- src/learners.erl
+++ src/learners.erl
@@ -1,6 +1,19 @@
 -module(learners).
+-include_lib("accepted_record.hrl").
 
--export ([broadcast_result/1]).
+-export ([
+    broadcast_result/1,
+    deliver_accept/1
+]).
+
+deliver_accept(Accepted) ->
+    case gaoler:learners() of
+        [] -> erlang:errors(no_learners);
+        Learners ->
+            [spawn(fun() -> gen_event:notify(Learner, Accepted) end) ||
+        	Learner <- Learners]
+    end,
+    ok.
 
 broadcast_result(Value) ->
     case gaoler:learners() of
