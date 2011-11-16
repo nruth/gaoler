b0a979ec89b95cea39ed56eab8e06f35590824c1
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-11-16 19:20:30 +0100

    refactoring acceptor_test to use the generator-per-subject layout

--- test/acceptor_test.erl
+++ test/acceptor_test.erl
@@ -6,69 +6,78 @@
 -define(PROMISED(N), #state{promised=N}).
 -define(PROMISED_AND_ACCEPTED(N, A), #state{promised=N, accepted=A}).
 
-%% prepare requests / promises
-promise_sent_when_no_higher_promises_made_test() ->
-  Sender = nil,
-  InitialState = ?NO_PROMISES,
-  Result = acceptor:handle_call({prepare, 5}, Sender, InitialState),
-  ?assertMatch({reply, {promised, 5, no_value}, _}, Result).
 
-% since the proposer should know someone else has already prevented 
-% its round from succeeding and reuse of the same promised message
-% allows simpler acceptor
-highest_promise_sent_when_lower_prepare_requested_test() ->
-  Sender = nil,
-  InitialState = ?PROMISED(6),
-  Result = acceptor:handle_call({prepare, 5}, Sender, InitialState),
-  ?assertMatch({reply, {promised, 6, no_value}, _}, Result).
+promise_requests_test_() -> [
+    fun should_send_promise_when_no_higher_promises_made/0,
+    fun should_send_highest_promise_when_lower_prepare_received/0,
+    fun should_not_change_state_when_receiving_lower_prepare_request/0
+    ].
 
-promise_state_updated_by_higher_prepare_test() ->
-  Sender = nil,
-  InitialState = ?NO_PROMISES,
-  Result = acceptor:handle_call({prepare, 5}, Sender, InitialState),
-  ?assertMatch({_, _, #state{promised = 5}}, Result).
+    should_send_promise_when_no_higher_promises_made() ->
+        Sender = nil,
+        InitialState = ?NO_PROMISES,
+        Result = acceptor:handle_call({prepare, 5}, Sender, InitialState),
+        ?assertMatch({reply, {promised, 5, no_value}, _}, Result).
 
-promise_state_unchanged_by_lower_prepare_test() ->
-  Sender = nil,
-  InitialState = ?PROMISED(6),
-  Result = acceptor:handle_call({prepare, 5}, Sender, InitialState),
-  ?assertMatch({_, _, #state{promised = 6}}, Result).
+    should_send_highest_promise_when_lower_prepare_received() ->
+        % since the proposer should know someone else has already prevented 
+        % its round from succeeding and reuse of the same promised message
+        % allows simpler acceptor
+        Sender = nil,
+        InitialState = ?PROMISED(6),
+        Result = acceptor:handle_call({prepare, 5}, Sender, InitialState),
+        ?assertMatch({reply, {promised, 6, no_value}, _}, Result).
 
-
-%% accept requests / votes updates state
-proposal_with_higher_round_than_promised_updates_accepted_test() ->
-  Sender = nil, 
-  InitialState = ?PROMISED_AND_ACCEPTED(4, prev),
-  Proposal = {accept, 5, v},
-  Result = acceptor:handle_call(Proposal, Sender, InitialState),
-  ?assertMatch({_, _, #state{accepted = {5,v}}}, Result).
-
-proposal_with_lower_round_than_promised_does_not_change_accepted_test() ->
-  Sender = nil, 
-  InitialState = ?PROMISED_AND_ACCEPTED(6, prev),
-  Proposal = {accept, 5, v},
-  Result = acceptor:handle_call(Proposal, Sender, InitialState),
-  ?assertMatch({_, _, #state{accepted = prev}}, Result).
+    should_not_change_state_when_receiving_lower_prepare_request() ->
+        Sender = nil,
+        InitialState = ?PROMISED(6),
+        Result = acceptor:handle_call({prepare, 5}, Sender, InitialState),
+        ?assertMatch({_, _, #state{promised = 6}}, Result).
 
 
-%% test acceptors reply to the proposer (no separate learners)
-proposal_with_higher_round_than_promised_replies_accept_test() ->
-  Sender = nil,
-  InitialState = ?PROMISED(3),
-  Proposal = {accept, 5, v},
-  Result = acceptor:handle_call(Proposal, Sender, InitialState),
-  ?assertMatch({reply, {accept, 5, v}, _}, Result).
+accept_request_state_changes_test_() -> [
+    fun should_update_accepted_when_higher_round_accept_requested/0,
+    fun should_not_change_accepted_when_lower_round_accept_requested/0
+].
 
-proposal_with_lower_round_than_promised_replies_reject_test() ->
-  Sender = nil,
-  InitialState = ?PROMISED(6),
-  Proposal = {accept, 5, v},
-  Result = acceptor:handle_call(Proposal, Sender, InitialState),
-    ?assertMatch({reply, {reject, 5}, _}, Result).
+    should_update_accepted_when_higher_round_accept_requested() ->
+        Sender = nil, 
+        InitialState = ?PROMISED_AND_ACCEPTED(4, prev),
+        Proposal = {accept, 5, v},
+        Result = acceptor:handle_call(Proposal, Sender, InitialState),
+        ?assertMatch({_, _, #state{accepted = {5,v}}}, Result).
 
-proposal_with_same_round_as_promised_replies_accept_test() ->
-    Sender = nil,
-    InitialState = ?PROMISED(5),
-    Proposal = {accept, 5, v},
-    Result = acceptor:handle_call(Proposal, Sender, InitialState),
-    ?assertMatch({reply, {accept, 5, v}, _}, Result).
+    should_not_change_accepted_when_lower_round_accept_requested() ->
+        Sender = nil, 
+        InitialState = ?PROMISED_AND_ACCEPTED(6, prev),
+        Proposal = {accept, 5, v},
+        Result = acceptor:handle_call(Proposal, Sender, InitialState),
+        ?assertMatch({_, _, #state{accepted = prev}}, Result).
+
+
+accept_request_replies_test_() -> [
+    fun should_reply_accept_for_promised_round/0,
+    fun should_reply_accept_for_round_higher_than_promise/0,
+    fun should_reply_reject_to_lower_round_than_promised/0
+].
+
+    should_reply_accept_for_promised_round() ->
+        Sender = nil,
+        InitialState = ?PROMISED(5),
+        Proposal = {accept, 5, v},
+        Result = acceptor:handle_call(Proposal, Sender, InitialState),
+        ?assertMatch({reply, {accept, 5, v}, _}, Result).
+
+    should_reply_accept_for_round_higher_than_promise() ->
+        Sender = nil,
+        InitialState = ?PROMISED(3),
+        Proposal = {accept, 5, v},
+        Result = acceptor:handle_call(Proposal, Sender, InitialState),
+        ?assertMatch({reply, {accept, 5, v}, _}, Result).
+
+    should_reply_reject_to_lower_round_than_promised() ->
+        Sender = nil,
+        InitialState = ?PROMISED(6),
+        Proposal = {accept, 5, v},
+        Result = acceptor:handle_call(Proposal, Sender, InitialState),
+        ?assertMatch({reply, {reject, 5}, _}, Result).
