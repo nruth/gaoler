9431aaf0e1edea1d31442108390c060a7f577235
Marcus Ljungblad <marcus@ljungblad.nu>
2011-12-01 17:07:16 +0100

    refactor for cleaner code

--- src/acceptor.erl
+++ src/acceptor.erl
@@ -58,33 +58,41 @@ handle_call({prepare, {ElectionId, Round}}, _From, State) ->
 handle_call({accept, {ElectionId, Round}, Value}, _From, State) ->
     handle_accept({ElectionId, Round}, Value, State).
 
+%%%===================================================================
+%%% Prepare requests
+%%%=================================================================== 
 handle_prepare({ElectionId, Round}, State) ->
     case lists:keyfind(ElectionId, 1, State#state.elections) of
         {ElectionId, FoundElection} ->
-            HighestPromise = max(Round, FoundElection#election.promised),
-            NewElection = FoundElection#election{promised = HighestPromise},
-            NewState = update_election(ElectionId, NewElection, State),
-            Reply = {promised, HighestPromise, NewElection#election.accepted},
-            persister:remember_promise(ElectionId, Round),
-            {reply, Reply, NewState};
+            handle_prepare_for_existing_election(ElectionId, Round, FoundElection, State);
         false ->
-            NewElection = #election{promised = Round},
-            NewState = add_new_election(ElectionId, NewElection, State),
-            persister:remember_promise(ElectionId, Round),
-            {reply, {promised, Round, NewElection#election.accepted}, NewState}
+            create_new_election_from_prepare_request(ElectionId, Round, State)
     end.    
 
+handle_prepare_for_existing_election(ElectionId, Round, FoundElection, State) ->
+    HighestPromise = max(Round, FoundElection#election.promised),
+    NewElection = FoundElection#election{promised = HighestPromise},
+    NewState = update_election(ElectionId, NewElection, State),
+    Reply = {promised, HighestPromise, NewElection#election.accepted},
+    persister:remember_promise(ElectionId, NewElection#election.promised),
+    {reply, Reply, NewState}.
+
+create_new_election_from_prepare_request(ElectionId, Round, State) ->
+    NewElection = #election{promised = Round},
+    NewState = add_new_election(ElectionId, NewElection, State),
+    persister:remember_promise(ElectionId, Round),
+    {reply, {promised, Round, NewElection#election.accepted}, NewState}.
+
+%%%===================================================================
+%%% Accept requests
+%%%=================================================================== 
 handle_accept({ElectionId, Round}, Value, State) ->
     {Reply, NextState} = 
         case lists:keyfind(ElectionId, 1, State#state.elections) of
             {ElectionId, _ElectionRecord}=Election ->
                 handle_accept_for_election(Round, Value, Election, State);
             false ->
-                NewElection = #election{promised = Round,
-                                        accepted = {Round, Value}},
-                NewState = add_new_election(ElectionId, NewElection, State),
-                persister:remember_vote(ElectionId, Round, Value),
-                {{accepted, Round, Value}, NewState}
+                create_new_election_from_accept_request(ElectionId, Round, Value, State)
         end,
     {reply, Reply, NextState}.
 
@@ -98,6 +106,17 @@ handle_accept_for_election(Round, Value, {ElectionId, Election}, State)
 handle_accept_for_election(Round, _Value, _Election, State) ->
     {{reject, Round}, State}.
 
+create_new_election_from_accept_request(ElectionId, Round, Value, State) ->
+    NewElection = #election{promised = Round,
+                            accepted = {Round, Value}},
+    NewState = add_new_election(ElectionId, NewElection, State),
+    persister:remember_vote(ElectionId, Round, Value),
+    {{accepted, Round, Value}, NewState}.
+
+
+%%%===================================================================
+%%% Internal functions
+%%%=================================================================== 
 add_new_election(ElectionId, NewElection, State) ->
     State#state{elections = [{ElectionId, NewElection}|
                              State#state.elections]}.
@@ -108,7 +127,6 @@ update_election(ElectionId, NewElection, State) ->
                          State#state.elections,
                          {ElectionId, NewElection}),
     State#state{elections = UpdatedElections}.
-
 
 %%%===================================================================
 %%% Uninteresting gen_server boilerplate
