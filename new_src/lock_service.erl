2fce07a94ae52771d14ac5f99b1ea563d4e61bce
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-12-03 00:04:08 +0100

    revised lock api

--- new_src/lock_service.erl
+++ new_src/lock_service.erl
@@ -20,26 +20,25 @@ start_link() ->
 % execute Critical fun when lock for Resource is acquired
 % and release the lock when Critical returns
 % fun()::Critical is executed by the calling process
-acquire(Resource, Critical, Timeout) where is_function(Critical, 0) ->
+run_with_mutex(Resource, Critical) where is_function(Critical, 0) ->
+    Ticket = acquire(Resource),
+    Critical(),
+    release(Resource).
+
+% blocking lock request
+acquire(Resource)
     % insert into lock queue
     % async await callback
     % run critical code (fun) on receiving callback 
     % release lock
     wait = gen_server:call({join_queue, Resource}),
     receive
-        {lock, Resource} -> Critical();
-    after
-        Timeout -> timeout
+        {lock, Resource} -> ok
     end.
 
-
-release(Ticket) ->
-    gen_server:call({advance_queue, lib_ticket:resource(Ticket)}),
-    receive
-        {lock, Resource} -> apply(Critical, []);
-    after
-        Timeout -> timeout
-    end.
+% release lock
+release(Resource) ->
+    gen_server:call({advance_queue, Resource}).
 
 stop() ->
     gen_server:cast(?SERVER, stop).
@@ -58,6 +57,7 @@ handle_call({join_queue, Resource}, From, State) ->
 
 handle_call({advance_queue, Resource}, _From, State) ->
     NewState = State#state{queue = advance_queue(State#state.queue)},
+    notify_new_lock_holder(State),
     Reply = ok,
     {reply, Reply, NewState}.
 
