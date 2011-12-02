9a388df101ce8e815709f591d818c93f32b3296f
Marcus Ljungblad <marcus@ljungblad.nu>
2011-12-02 15:00:31 +0100

    log file name contains node name too

--- include/persister.hrl
+++ include/persister.hrl
@@ -1,3 +1,5 @@
 -define(LOGFILE, "logfile.log").
 -define(LOGDIRECTORY, "acceptor_logs").
--define(LOG(Election), ?LOGDIRECTORY++"/"++integer_to_list(Election)++".log").
+-define(LOG(Election), ?LOGDIRECTORY++
+            "/"++integer_to_list(Election)++"_"
+            ++atom_to_list(node())++".log").
