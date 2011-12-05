986ad715e284b1939e5d36b897511a15ac5c85f4
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-12-05 15:53:50 +0100

    centralised lock service

---
+++ src/centralised_lock.erl
@@ -0,0 +1,75 @@
+-module (centralised_lock).
+-export ([acquire/1, release/1, get_queue/0]).
+
+-behaviour(gen_server).
+-export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record (state, {queue = undefined}).
+
+%%% API
+start_link() ->
+    gen_server:start_link({local, lock_service}, ?MODULE, [], []).
+
+acquire (Client) ->
+    gen_server:call(lock_service, {acquire, Client}).
+
+release (Client) ->
+    gen_server:call(lock_service, {release, Client}).
+
+get_queue() -> 
+    gen_server:call(lock_service, get_queue).
+
+%%% internals
+
+% starts with empty data store
+init([]) -> {ok, #state{queue = queue:new()}}.
+
+handle_call({acquire, Client}, _From, State) ->
+    {reply, ok, acquire(Client, State)};
+handle_call(get_queue, _From, State) ->
+    {reply, State#state.queue, State};
+handle_call({release, Client}, _From, State) -> 
+    {reply, ok, release(Client, State)}.
+
+handle_cast(stop, State) -> 
+    {stop, normal, State}.
+
+%% add the client to the lock queue, and
+%% give them the lock if nobody else was waiting
+acquire(Client, State) ->
+    Queue = State#state.queue,
+    case queue:is_empty(Queue) of
+        true -> send_lock(Client);
+        false -> noop
+    end,
+    NewQueue = queue:in(Client, Queue),
+    State#state{queue=NewQueue}.
+
+%% give the current lock holder from the queue
+%%  and give the lock to the next in queue (if any)
+release(_Client, State) ->
+    case queue:out(State#state.queue) of
+        {{value, _Releasing}, NewQueue} ->
+            %remove lock, send new if any
+            case queue:peek(NewQueue) of
+                {value, NextLockHolder} ->
+                    send_lock(NextLockHolder);
+                empty -> 
+                    noop
+            end,
+            State#state{queue=NewQueue};
+        {empty, EmptyQueue} -> % no lock is held, do nothing
+            State#state{queue=EmptyQueue}
+    end.
+
+
+send_lock(Receiver) ->
+    Receiver ! lock.
+
+
+%%%===================================================================
+%%% Uninteresting gen_server boilerplate
+%%%===================================================================
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
