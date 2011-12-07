73e397a54851fe0baa03989828fa348870ba4185
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-12-07 15:56:07 +0100

    fixing replica decision handling and prop gc

--- src/replica.erl
+++ src/replica.erl
@@ -36,8 +36,7 @@ loop(State) ->
             io:format("proposing ~p~n", [Command]),
             loop(NewState);
         {decision, Slot, Command} ->
-            NewStateA = add_decision_to_state({Slot, Command}, State),
-            NewStateB = consume_decisions(NewStateA),
+            handle_decision(Slot, Command, State),
             loop(NewStateB);
         {'EXIT', _FromPid, _Reason} ->
             'a proposal crashed or exited',
@@ -60,14 +59,15 @@ propose(Command, State) ->
             State
     end.
 
-%% returns the next available slot, using the local replica's state
+%% returns the next available command sequence slot, using the local replica's state
 slot_for_next_proposal(#replica{proposals=Proposals, decisions=Decisions}) ->
     MaxSlotFn = fun({Slot, _Command}, Highest) -> max(Slot, Highest) end,
     MaxPropSlot = lists:foldl(MaxSlotFn, 0, Proposals),
     HighSlot = lists:foldl(MaxSlotFn, MaxPropSlot, Decisions),
     1 + HighSlot.
 
-
+%% predicate returning true/false 
+%% whether the command has already been decided and applied to the replica state
 is_command_already_decided(Command, State) ->
     lists:any(
         fun({_S, DecidedCommand}) -> 
@@ -76,25 +76,41 @@ is_command_already_decided(Command, State) ->
         State#replica.decisions
     ).
 
+handle_decision(Slot, Command, State) ->
+    NewStateA = add_decision_to_state({Slot, Command}, State),
+    consume_decisions(NewStateA).
+
+%% Performs as many decided (queued) commands as possible
+%%   * Delivers contiguous commands from the holdback queue, 
+%%      halting when reaching an empty slot
+%%   * checks whether any proposals were pre-empted
+%% returns: updated state, with changes to application, slot_number, and the command queues
 consume_decisions(State) ->
-    case lists:keyfind(State#replica.slot_num, 1, State#replica.decisions) of
+    Slot = State#replica.slot_num,
+    case lists:keyfind(Slot, 1, State#replica.decisions) of
         false ->
             State;
-        {_Slot, Command} ->
-            % perform as many decided (queued) commands as possible
-            % also check if any of our proposals have collided with another
-            perform(Command, handle_proposal_preemption(State))
+        {Slot, DecidedCommand} ->
+            StateAfterProposalGC = gc_proposals(Slot, DecidedCommand, State)
+            StateAfterPerform = perform(DecidedCommand, StateAfterProposalGC),
+            consume_decisions(StateAfterPerform)
     end.
 
-handle_proposal_preemption(State) ->
-    % if someone stole our slot, propose again
-    case lists:keyfind(State#replica.slot_num, 1, State#replica.proposals) of 
+%% Garbage-collects the proposal for the given slot
+%% When the command does not match the decided command
+%% it will be re-proposed for another slot
+gc_proposals(Slot, DecidedCommand, State) ->
+    case lists:keyfind(Slot, 1, State#replica.proposals) of 
         false ->
             State;
-        {_Slot, ConflictingCommand} ->
-            propose(ConflictingCommand, State)
+        {Slot, DecidedCommand} ->
+            % leave 
+            State;
+        {Slot, ConflictingCommand} ->
+            CleanedState = remove_proposal_from_state(Slot, State)
+            propose(ConflictingCommand, CleanedState)
     end.
-    
+
 perform({ClientProxy, UniqueRef, {Operation, Client}}, State) ->
     FunFilter = fun({S, _Operation}) -> S < State#replica.slot_num end,
     case lists:any(FunFilter, State#replica.decisions) of
@@ -115,6 +131,9 @@ add_decision_to_state(SlotCommand, State) ->
 
 add_proposal_to_state(Proposal, State) ->
     State#replica{ proposals = [Proposal | State#replica.proposals] }.
+
+remove_proposal_from_state(SlotNumber, State) ->
+    State#replica{proposals = lists:keydelete(SlotNumber, State#replica.proposals)}.
 
 send_to_leaders(Proposal, _State) ->
     proposer:propose(Proposal).
