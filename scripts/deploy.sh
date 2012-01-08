e6fdaa837eba583662d0d7a52faab234b8a4e25f
Marcus Ljungblad <marcus@ljungblad.nu>
2012-01-09 00:26:39 +0100

    minor fix to get dependencies in rebar

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-./execute.sh 'cd gaoler; cp ebin/gaoler.app ~/gaoler.app; git pull origin master; rel/gaoler/bin/gaoler stop; rm -rf rel/gaoler; ./rebar compile; cp ~/gaoler.app ebin/gaoler.app; ./rebar generate; rel/gaoler/bin/gaoler start'
+./execute.sh 'cd gaoler; cp ebin/gaoler.app ~/gaoler.app; git pull origin master; rel/gaoler/bin/gaoler stop; rm -rf rel/gaoler; ./rebar get-deps; ./rebar compile; cp ~/gaoler.app ebin/gaoler.app; ./rebar generate; rel/gaoler/bin/gaoler start'
