c07946de721b5328c8c5fc3b4da3382050f3524f
Nicholas Rutherford <nick.rutherford@gmail.com>
2012-01-08 15:33:15 +0100

    completing lock_test

--- test/lock_test.erl
+++ test/lock_test.erl
@@ -20,7 +20,9 @@ teardown(Mods) ->
     meck:unload(Mods).
 
 add_dummy_client_to_queue(State) ->
-    State#state{queue = queue:in(dummy_client, State#state.queue)}.
+    add_client_to_queue(State, dummy_client).
+add_client_to_queue(State, Client) ->    
+    State#state{queue = queue:in(Client, State#state.queue)}.
 
 
 %%% =============================
@@ -83,3 +85,45 @@ with_queued_clients_dont_run_persistence_lock_granted_callback() ->
     ClientsQueuedState = add_dummy_client_to_queue(InitState),
     {reply, ok, FinState} = lock:handle_call({acquire, someclient}, sender, ClientsQueuedState),
     ?assertNot(meck:called(mock_lock_persistence, lock_granted, [FinState])).
+
+%%% =============================
+%%% Release lock
+%%% =============================
+behaviour_on_release_request_test_() -> {foreach, fun setup/0, fun teardown/1, [
+    fun with_only_holder_queued_make_empty_queue/0,
+    fun with_only_holder_queued_run_persistence_lock_available_callback/0,
+    fun with_queued_clients_remove_releaser_from_queue/0,
+    fun with_queued_clients_send_lock_to_next_in_queue/0,
+    fun with_queued_clients_run_persistence_lock_holder_changed_callback/0
+]}.
+
+with_only_holder_queued_make_empty_queue() ->
+    {ok, InitState} = lock:init([mock_lock_persistence, mock_comms]),
+    HolderQueuedState = add_dummy_client_to_queue(InitState),
+    {reply, ok, FinState} = lock:handle_call({release, someclient}, sender, HolderQueuedState),
+    ?assert(queue:is_empty(FinState#state.queue)).
+
+with_only_holder_queued_run_persistence_lock_available_callback() ->
+    {ok, InitState} = lock:init([mock_lock_persistence, mock_comms]),
+    HolderQueuedState = add_dummy_client_to_queue(InitState),
+    {reply, ok, FinState} = lock:handle_call({release, someclient}, sender, HolderQueuedState),
+    ?assertNot(meck:called(mock_lock_persistence, lock_holder_changed, [FinState])).
+
+with_queued_clients_run_persistence_lock_holder_changed_callback() ->
+    {ok, InitState} = lock:init([mock_lock_persistence, mock_comms]),
+    ClientsQueuedState = add_client_to_queue(add_client_to_queue(InitState, a), b),
+    {reply, ok, FinState} = lock:handle_call({release, a}, sender, ClientsQueuedState),
+    ?assertNot(meck:called(mock_lock_persistence, lock_holder_changed, [FinState])).
+
+with_queued_clients_remove_releaser_from_queue() ->
+    {ok, InitState} = lock:init([mock_lock_persistence, mock_comms]),
+    ClientsQueuedState = add_client_to_queue(add_client_to_queue(InitState, a), b),
+    {reply, ok, FinState} = lock:handle_call({release, a}, sender, ClientsQueuedState),
+    ?assertNot(queue:member(a, FinState#state.queue)),
+    ?assertEqual(b, queue:get(FinState#state.queue)).
+
+with_queued_clients_send_lock_to_next_in_queue() ->
+    {ok, InitState} = lock:init([mock_lock_persistence, mock_comms]),
+    ClientsQueuedState = add_client_to_queue(add_client_to_queue(InitState, a), b),
+    {reply, ok, _FinState} = lock:handle_call({release, a}, sender, ClientsQueuedState),
+    ?assert(meck:called(mock_comms, send_lock, [b])).
