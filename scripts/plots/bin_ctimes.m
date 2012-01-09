19a44b94a002e9a336d10e84253d0e9352093ba9
Nicholas Rutherford <nick.rutherford@gmail.com>
2012-01-09 21:17:25 +0100

    adding semicolons to matlab scripts

--- scripts/plots/bin_ctimes.m
+++ scripts/plots/bin_ctimes.m
@@ -1,8 +1,8 @@
 function ResultTable = bin_ctimes( interval, CTimes)
     max_num = max(CTimes);
-    edges = 0:interval:max_num
-    BinnedCTimes = histc(CTimes, edges)
+    edges = 0:interval:max_num;
+    BinnedCTimes = histc(CTimes, edges);
     BinThroughputs = BinnedCTimes / interval;
-    ResultTable = [edges', BinThroughputs]
+    ResultTable = [edges', BinThroughputs];
 end
 
