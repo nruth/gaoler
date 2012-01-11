65bbe9083255176dd33a51c1d2edba208481cb83
Nicholas Rutherford <nick.rutherford@gmail.com>
2012-01-12 00:12:42 +0100

    persistence callbacks -> gen_server with file kept open

--- src/lock_persist_holder.erl
+++ src/lock_persist_holder.erl
@@ -2,28 +2,54 @@
 % when no holder sets to none
 -module (lock_persist_holder).
 -export ([lock_granted/1, lock_available/1, lock_holder_changed/1, init/0]).
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %% called to perform any start-up procedure for the lock persistence
 %% e.g. create dets table
 init() ->
-    ok.
+    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
+    {ok, FileHandle} = file:open(lock_queue_store, [raw, write]), % overwrites, not append
+    {ok, FileHandle}.
 
 %% called when the lock is held by the server, 
 %% and is being issued to a client
-lock_granted(Queue) -> 
-    ok = atomic_persist({lock_holder, queue:peek(Queue)}).
+lock_granted(Queue) ->
+    ok = gen_server:call(?MODULE, {write, {lock_holder, queue:peek(Queue)}}).
 
 %% called when the lock is held by the server, ready for issue
 lock_available(_Queue) -> 
-    ok = atomic_persist({lock_holder, none}).
+    ok = gen_server:call(?MODULE, {write, {lock_holder, none}}).
 
 %% called when the lock is released by a client
 %% and another client is queueing for it
 %% so they will be issued it immediately
 lock_holder_changed(Queue) -> 
-    ok = atomic_persist({lock_holder, queue:peek(Queue)}).
+    ok = gen_server:call(?MODULE, {write, {lock_holder, queue:peek(Queue)}}).
 
-atomic_persist(Term) ->
-    {ok, FileHandle} = file:open(lock_queue_store, [raw, write]), % overwrites, not append
+
+%%% INTERNALS
+
+handle_call({write, Term}, _From, FileHandle) ->
+    atomic_persist(Term, FileHandle),
+    {reply, ok, FileHandle}.
+
+atomic_persist(Term, FileHandle) ->
     file:write(FileHandle, Term),
+    file:datasync(FileHandle).
+
+handle_cast(stop, State) -> 
+    {stop, normal, State}.
+
+terminate(_Reason, FileHandle) -> 
     file:close(FileHandle).
+
+
+%%%===================================================================
+%%% Uninteresting gen_server boilerplate
+%%%===================================================================
+handle_info(_Info, State) -> {noreply, State}.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
--- src/lock_persist_queue.erl
+++ src/lock_persist_queue.erl
@@ -3,27 +3,56 @@
 -module (lock_persist_queue).
 -export ([lock_granted/1, lock_available/1, lock_holder_changed/1, init/0]).
 
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+
 %% called to perform any start-up procedure for the lock persistence
 %% e.g. create dets table
 init() ->
-    ok.
+    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
+    {ok, FileHandle} = file:open(lock_queue_store, [raw, write]), % overwrites, not append
+    {ok, FileHandle}.
+
 
 %% called when the lock is held by the server, 
 %% and is being issued to a client
 lock_granted(Queue) -> 
-    atomic_persist(Queue).
+    ok = gen_server:call(?MODULE, {write, Queue}).
 
 %% called when the lock is held by the server, ready for issue
 lock_available(Queue) -> 
-    atomic_persist(Queue).
+    ok = gen_server:call(?MODULE, {write, Queue}).
 
 %% called when the lock is released by a client
 %% and another client is queueing for it
 %% so they will be issued it immediately
 lock_holder_changed(Queue) -> 
-    atomic_persist(Queue).
+    ok = gen_server:call(?MODULE, {write, Queue}).
 
-atomic_persist(Term) ->
-    {ok, FileHandle} = file:open(lock_queue_store, [raw, write]), % overwrites, not append
+
+%%% INTERNALS
+
+handle_call({write, Term}, _From, FileHandle) ->
+    atomic_persist(Term, FileHandle),
+    {reply, ok, FileHandle}.
+
+atomic_persist(Term, FileHandle) ->
     file:write(FileHandle, Term),
+    file:datasync(FileHandle).
+
+handle_cast(stop, State) -> 
+    {stop, normal, State}.
+
+terminate(_Reason, FileHandle) -> 
     file:close(FileHandle).
+
+
+%%%===================================================================
+%%% Uninteresting gen_server boilerplate
+%%%===================================================================
+handle_info(_Info, State) -> {noreply, State}.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
