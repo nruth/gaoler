5bfb37be206bb9d390a0dab8c542492012004f5e
Nicholas Rutherford <nick.rutherford@gmail.com>
2012-01-12 16:04:08 +0100

    updating deploy script comments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# why?
+# prevent new release overwriting the node settings with repo defaults
 ./execute.sh 'cd ~emdc/gaoler && cp ebin/gaoler.app ~/gaoler.app'
 
 
@@ -11,8 +11,8 @@
 # rebuild
 ./execute.sh 'cd ~emdc/gaoler && git pull origin master && ./rebar get-deps && ./rebar compile'
 
-# why?
+# prevent new release overwriting the node settings with repo defaults
 ./execute.sh 'cd ~emdc/gaoler && cp ~/gaoler.app ebin/gaoler.app'
 
 # generate and run the release
-./execute.sh 'cd ~emdc/gaoler && ./rebar generate && rel/gaoler/bin/gaoler start'
+# ./execute.sh 'cd ~emdc/gaoler && ./rebar generate && rel/gaoler/bin/gaoler start'
