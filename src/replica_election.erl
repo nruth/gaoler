dbce36abbb6cb3a13c49995f7c7b36ddf01b841b
Nicholas Rutherford <nick.rutherford@gmail.com>
2012-01-12 23:18:29 +0100

    localhost() -> node()

--- src/replica_election.erl
+++ src/replica_election.erl
@@ -2,7 +2,7 @@
 -export ([am_i_a_replica/1, is_replica/3]).
 
 am_i_a_replica(HowManyReplicas) ->
-    is_replica(nodes(), net_adm:localhost(), HowManyReplicas).
+    is_replica(nodes(), node(), HowManyReplicas).
 
 is_replica(Nodes, Node, HowManyReplicas) ->
     Node =< lists:nth(HowManyReplicas, lists:sort(Nodes)).
