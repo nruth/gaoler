94886ae9b3a72c81cf1f54febdbcba5727d1ae69
Nicholas Rutherford <nick.rutherford@gmail.com>
2012-01-12 23:27:39 +0100

    adding replica_list

--- src/replica_election.erl
+++ src/replica_election.erl
@@ -1,8 +1,18 @@
--module (replica_election).
--export ([am_i_a_replica/1, is_replica/3]).
+-module (replicas).
+-export ([am_i_a_replica/1, is_replica/3, replica_list/1]).
 
+
+%% predicate for whether the current node() is considered a replica
+%% assumes nodes() already connected
 am_i_a_replica(HowManyReplicas) ->
     is_replica(nodes(), node(), HowManyReplicas).
 
+
+%% returns the list of nodes bearing replicas, a sublist of nodes()
+%% assumes nodes() already connected
+replica_list(HowManyReplicas) ->
+    lists:sublist(lists:sort([node()|nodes()]), HowManyReplicas).
+
+%internal but exported for testing
 is_replica(Nodes, Node, HowManyReplicas) ->
     Node =< lists:nth(HowManyReplicas, lists:sort(Nodes)).
